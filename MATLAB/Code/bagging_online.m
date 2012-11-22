5528b0f35509eb8cd9e9f11ffdcfe3ccdde245a8
Siddhant Goel <siddhantgoel@gmail.com>
2012-11-22 19:06:12 +0100

    Bugfix in online bagging (in calculting accuracy when there are only two points under consideration)

--- MATLAB/Code/bagging_online.m
+++ MATLAB/Code/bagging_online.m
@@ -24,6 +24,7 @@ sv_labels = cell(M, 1);
 % find two training instances with labels 1 and -1
 i = find(y_training == 1, 1);
 j = find(y_training == -1, 1);
+predictions = zeros(size(y_testing, 1), 1);
 for m = 1 : M
     % train the model
     learners{m} = svmtrain(ones(2, 1), [y_training(i, :); y_training(j, :);], [x_training(i, :); x_training(j, :);], param);
@@ -31,9 +32,10 @@ for m = 1 : M
     sv_instances{m} = learners{m}.SVs;
     sv_labels{m} = svmpredict(ones(2, 1), sv_instances{m}, learners{m});
     % predict accuracy on the testing data
-    [~, a, ~] = svmpredict(y_testing, x_testing, learners{m});
-    accuracy(1) = a(1) / 100;
+    predictions(:, m) = svmpredict(y_testing, x_testing, learners{m});
 end
+predictions = sign(sum(predictions, 2));
+accuracy(1) = sum(predictions == y_testing) / size(y_testing, 1);
 x_training(i, :) = []; y_training(i, :) = [];
 x_training(j, :) = []; y_training(j, :) = [];
 
