82702b62a6986984d22e7f34dc5407d8300bf168
Siddhant Goel <siddhantgoel@gmail.com>
2012-12-07 13:12:21 +0100

    Replace cross_validation.cross_val_score with cross_validation.train_test_split

--- Python/Code/MLmodels/svm.py
+++ Python/Code/MLmodels/svm.py
@@ -5,17 +5,34 @@ import util
 from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import cross_validation
+from pprint import pprint
 
 def main():
+    # initialize the global data
     labels, timestamps, comments = util.get_comments_data("Dataset/comments.csv")
     vec = TfidfVectorizer(ngram_range = (1, 2), stop_words = 'english')
     instances = vec.fit_transform(comments)
 
-    clf = LinearSVC(C = 1)
-    clf.fit(instances, labels)
+    cv = 5; cv_accuracy = list();
+    for idx in xrange(0, cv):
+        # initialize training/testing data
+        cv_data = cross_validation.train_test_split(instances, labels, test_size = 0.5, random_state = idx)
+        x_training = cv_data[0]
+        x_testing = cv_data[1]
+        y_training = cv_data[2]
+        y_testing = cv_data[3]
 
-    scores = cross_validation.cross_val_score(clf, instances, labels, cv = 5)
-    print scores
+        # train the classifier on the training data
+        clf = LinearSVC(C = 1)
+        clf.fit(x_training, y_training)
+
+        # test the classifer on the testing data
+        predictions = clf.predict(x_testing)
+        correct = sum(predictions == y_testing)
+        total = len(predictions)
+        cv_accuracy.append(float(correct) / float(total))
+
+    pprint(cv_accuracy)
 
 if __name__ == "__main__":
     main()
