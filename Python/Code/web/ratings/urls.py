9e440b75aa01cf936d20342ff5519a09768f5d3d
Siddhant Goel <siddhantgoel@gmail.com>
2012-12-11 23:52:42 +0100

    Implement the rating page

--- Python/Code/web/ratings/urls.py
+++ Python/Code/web/ratings/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('ratings.views',
     (r'^$', 'index'),
-    (r'^/(?P<story_id>\d+)/rate/$', 'rate'),
-    (r'^/fetch/$', 'fetch'),
+    (r'^(?P<story_id>\d+)/rate/$', 'rate'),
 )
--- Python/Code/web/ratings/views.py
+++ Python/Code/web/ratings/views.py
@@ -1,19 +1,30 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, render_to_response, redirect
+from django.template import RequestContext
 
 from ratings.models import Story
 
 def index(request):
-    try:
-        story = Story.objects.get(label = 0)
-    except Story.DoesNotExist:
-        return render_to_response("ratings/index.html", { 'error_message': 'No unlabeled stories left' })
+    if Story.objects.filter(label = 0).count() > 0:
+        story = Story.objects.filter(label = 0)[0]
+        return render_to_response(
+                "ratings/index.html",
+                { 'story': story },
+                context_instance = RequestContext(request)
+        )
     else:
-        return render_to_response("ratings/index.html", { 'story': story })
+        return render_to_response(
+                "ratings/index.html",
+                { 'error_message': 'No unlabeled stories left' },
+                context_instance = RequestContext(request)
+        )
 
 def rate(request, story_id):
-    story = get_object_or_404(pk = story_id)
-    if request.POST['label']:
-        story.label = int(request.POST['label'])
+    story = get_object_or_404(Story, pk = story_id)
+    if request.POST[u'label']:
+        if request.POST[u'label'] == u'depressed':
+            story.label = 1
+        elif request.POST[u'label'] == u'happy':
+            story.label = -1
         story.save()
-    return redirect('index')
+    return redirect("/ratings/")
