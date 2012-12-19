65eb396101d46b68e31f34148a9dec0f67530dae
Siddhant Goel <siddhantgoel@gmail.com>
2012-12-19 17:38:41 +0100

    Serve static content from within django

--- Python/Code/web/web/urls.py
+++ Python/Code/web/web/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from web import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,4 +9,5 @@ urlpatterns = patterns('',
     url(r'^ratings/', include('ratings.urls')),
     url(r'^monitor/', include('monitor.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
 )
