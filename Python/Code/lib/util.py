7cd66e3f89f352b6a30c77292bf100a9b6047bd4
Siddhant Goel <siddhantgoel@gmail.com>
2012-12-27 00:42:40 +0100

    Add a utility function to read the depression dataset from MySQL

--- Python/Code/lib/util.py
+++ Python/Code/lib/util.py
@@ -1,5 +1,6 @@
-# Utility functions go here
+import _mysql
 
+# load the kaggle comments dataset
 def get_comments_data(filename):
     f = open(filename)
     labels, timestamps, comments = list(), list(), list()
@@ -15,4 +16,24 @@ def get_comments_data(filename):
     f.close()
     comments = map(lambda x: x.strip('\n').strip('"""'), comments)
     return (labels, timestamps, comments)
+
+# load the depression dataset
+def get_distress_data(connection):
+    db = _mysql.connect(
+            connection["hostname"],
+            connection["database"],
+            connection["username"],
+            connection["password"],
+    )
+    db.query("select * from ratings_story")
+    labels, stories = list(), list()
+    results = db.store_result()
+    while True:
+        row = results.fetch_row()
+        if not row:
+            break
+        else:
+            labels.append(int(row[0][3]))
+            stories.append(row[0][2])
+    return (labels, stories)
 
