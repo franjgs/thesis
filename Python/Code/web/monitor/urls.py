444000dbad3653473d614d9c0ddd895c89f7c5f9
Siddhant Goel <siddhantgoel@gmail.com>
2013-01-04 15:42:36 +0100

    Skeleton code for monitoring different models

--- Python/Code/web/monitor/urls.py
+++ Python/Code/web/monitor/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('monitor.views',
     (r'^$', 'index'),
+    (r'^(?P<name>\w+)/$', 'stats'),
 )
--- Python/Code/web/monitor/views.py
+++ Python/Code/web/monitor/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render_to_response
 
 def index(request):
     return render_to_response("monitor/index.html", {})
+
+def stats(request, name):
+    context = None
+    if name == "svm":
+        context = { 'name': 'SVM' }
+    else:
+        context = { 'name': name.capitalize }
+    return render_to_response("monitor/index.html", context)
