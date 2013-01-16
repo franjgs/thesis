0fa47fed16b8daa91f5064a8ceb72ffbfa3be266
Siddhant Goel <siddhantgoel@gmail.com>
2013-01-16 15:23:03 +0100

    Change to boot script for gunicorn

--- Python/Code/web/boot.sh
+++ Python/Code/web/boot.sh
@@ -1,3 +1,16 @@
 #! /usr/bin/env bash
 
-gunicorn_django --workers=1 --log-level=info --bind 0.0.0.0:10001
+set -e
+
+# setup variables
+LOGFILE=/home/siddhant/thesis/Python/Code/web/gunicorn.log
+LOGDIR=$(dirname $LOGFILE)
+NUM_WORKERS=1
+USER=siddhant
+GROUP=siddhant
+
+# start the main application
+cd /home/siddhant/thesis/Python/Code/web/
+pythonbrew venv use thesis
+test -d $LOGDIR || mkdir -p $LOGDIR
+exec gunicorn_django -w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE
