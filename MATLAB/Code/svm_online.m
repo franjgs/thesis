0aa17868ab71ecacbba0c235dd88a115943079e2
Siddhant Goel <siddhantgoel@gmail.com>
2013-02-27 21:52:10 +0100

    Add code to plot number of support vectors in Online SVM (MATLAB)

--- MATLAB/Code/svm_online.m
+++ MATLAB/Code/svm_online.m
@@ -4,10 +4,14 @@ load('seeds.mat'); rng(s);
 
 [labels_global, instances_global] = libsvmread('Data/n-gram.data');
 
+to_plot = 'n_sv'; % or accuracy
+
 S = 100; N = round(size(instances_global, 1) / S) + 1;
 labels = []; instances = [];
 accuracy_x = zeros(N + 1, 1); accuracy_y = zeros(N + 1, 1);
 accuracy_x(1) = 0; accuracy_y(1) = 0;
+n_sv_x = zeros(N + 1, 1); n_sv_y = zeros(N + 1, 1);
+n_sv_x(1) = 0; n_sv_y(1) = 0;
 params = '-t 0 -c 1 -h 0 -w1 %.3f -w-1 %.3f';
 % while all samples not processed
 for i = 1 : N
@@ -40,11 +44,19 @@ for i = 1 : N
     % get predictions
     [predictions, ~, ~] = svmpredict(y_testing, x_testing, model);
     
-    % get accuracy
+    % get accuracy and number of support vectors
     accuracy_x(i + 1) = size(instances, 1);
     accuracy_y(i + 1) = 100 * sum(predictions == y_testing) / size(y_testing, 1);
+    n_sv_x(i + 1) = size(instances, 1);
+    n_sv_y(i + 1) = model.totalSV;
 end
 
-plot(accuracy_x, accuracy_y, 'bd-');
-axis([0 size(instances, 1) 0 100]);
-legend('Accuracy'); xlabel('Number of instances'); ylabel('Accuracy (%)');
+if strcmp(to_plot, 'accuracy') == 1
+    plot(accuracy_x, accuracy_y, 'bd-');
+    axis([0 size(instances, 1) 0 100]);
+    legend('Accuracy'); xlabel('Number of instances'); ylabel('Accuracy (%)');
+elseif strcmp(to_plot, 'n_sv') == 1
+    plot(n_sv_x, n_sv_y, 'bd-');
+    axis([0 size(instances, 1) 0 max(n_sv_y) + 1000]);
+    legend('Number of Support Vectors'); xlabel('Number of instances'); ylabel('Number of Support Vectors');
+end
