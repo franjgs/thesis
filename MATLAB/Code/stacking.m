ea088acc24f6f21f3302939d7a30942e226d3d4c
Siddhant Goel <siddhantgoel@gmail.com>
2013-03-03 14:25:26 +0100

    Fix stacking MATLAB code

--- MATLAB/Code/stacking.m
+++ MATLAB/Code/stacking.m
@@ -2,52 +2,51 @@ clear; close all;
 
 load('seeds.mat'); rng(s);
 
-[labels, instances] = libsvmread('Data/a1a.data');
-
-n_global = size(instances, 1);
+[labels, instances] = libsvmread('Data/n-gram.data');
 
 cv = cvpartition(labels, 'Kfold', 10);
 cv_accuracy = zeros(1, cv.NumTestSets);
 
-M = 10;
+M = 9;
+params = '-t 0 -c 1 -h 0 -w1 %.3f -w-1 %.3f';
 
-for idx = 1 : cv.NumTestSets
-    fprintf('Iteration #%d\n', idx);
+for i = 1 : cv.NumTestSets
+    fprintf('Iteration #%d\n', i);
     
-    training = cv.training(idx);
-    testing = cv.test(idx);
+    training = cv.training(i);
+    testing = cv.test(i);
     
     % input data for the first level learners
     x_training = instances(training, :); y_training = labels(training, :);
     
     % train individual learners on the training data (first level)
-    learners = cell(M, 1);
-    idx_learner = 1;
-    w = ones(size(x_training, 1), 1);
-    positive = size(y_training, 1) / sum(y_training == 1);
-    negative = size(y_training, 1) / sum(y_training == -1);
-    for i = 1 : M
-        param = sprintf('-t 0 -c %s -w1 %.3f -w-1 %.3f', num2str(i * 10), positive, negative);
-        learners{idx_learner} = svmtrain(w, y_training, x_training, param);
-        idx_learner = idx_learner + 1;
+    models = cell(M, 1);
+    n = size(x_training, 1);
+    for m = 1 : M
+        indices = randsample(n, randi([round(n/2), n]));
+        w = ones(size(indices, 1), 1);
+        positive = numel(indices) / sum(y_training(indices, :) == 1);
+        negative = numel(indices) / sum(y_training(indices, :) == -1);
+        models{m} = svmtrain(w, y_training(indices, :), x_training(indices, :), sprintf(params, positive, negative));
     end
-    
-    m = size(learners, 1);
     
     % prepare the data for the second level learner using the outputs from
     % the first level learners
-    x = zeros(n_global, m);
+    x = zeros(size(instances, 1), m);
     y = labels;
-    for i = 1 : m
-        x(:, i) = svmpredict(labels, instances, learners{i});
+    for m = 1 : M
+        x(:, m) = svmpredict(labels, instances, models{m});
     end
     
     % train the second level learner
-    learner = svmtrain(w, y(training, :), x(training, :), '-t 0 -c 1');
+    w = ones(size(x(training, :), 1), 1);
+    positive = size(y(training, :), 1) / sum(y(training, :) == 1);
+    negative = size(y(training, :), 1) / sum(y(training, :) == -1);
+    model = svmtrain(w, y(training, :), x(training, :), sprintf(params, positive, negative));
     
     % test performance over the testing dataset
-    predictions = svmpredict(y(testing, :), x(testing, :), learner);
-    cv_accuracy(idx) = sum(predictions == y(testing, :)) / size(y(testing, :), 1);
+    predictions = svmpredict(y(testing, :), x(testing, :), model);
+    cv_accuracy(i) = sum(predictions == y(testing, :)) / size(y(testing, :), 1);
 end
 
 fprintf('Accuracy => [%s]\nMean => %s\n', num2str(cv_accuracy), num2str(mean(cv_accuracy)));
