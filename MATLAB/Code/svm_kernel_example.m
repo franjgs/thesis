c9255d01c97af8beb85e97ea6ee108eb9887d9c7
Siddhant Goel <siddhantgoel@gmail.com>
2013-03-04 19:17:46 +0100

    Fix plots for transformation functions (SVM description)

--- MATLAB/Code/svm_kernel_example.m
+++ MATLAB/Code/svm_kernel_example.m
@@ -11,7 +11,7 @@ y_in = r_in .* sin(theta);
 label_in = ones(n, 1);
 
 % points outside the circle
-r_out = radius + rand(n, 1) * radius;
+r_out = radius + 5 + rand(n, 1) * radius;
 x_out = r_out .* cos(theta);
 y_out = r_out .* sin(theta);
 label_out = -1 * ones(n, 1);
@@ -21,15 +21,15 @@ instances = [x_in y_in; x_out y_out;];
 labels = [label_in; label_out;];
 
 % plot the points in 2D
-scatter(instances(1:200, 1), instances(1:200, 2), 'r');
-hold on;
-scatter(instances(201:400, 1), instances(201:400, 2), 'b');
-hold off;
+% scatter(instances(1:200, 1), instances(1:200, 2), 'r');
+% hold on;
+% scatter(instances(201:400, 1), instances(201:400, 2), 'b');
+% hold off;
 
 % transform the input to 3D and plot again
-% instances = [instances ones(400, 1) .* (1:1:400)'];
-% scatter3(instances(1:200, 1), instances(1:200, 2), instances(1:200, 3), 'r');
-% hold on;
-% scatter3(instances(201:400, 1), instances(201:400, 2), instances(201:400, 3), 'b');
-% xlabel('X Axis'); ylabel('Y Axis'); zlabel('Z Axis'); grid on;
-% hold off;
+instances = [instances sqrt(power(instances(:, 1), 2) + power(instances(:, 2), 2))];
+scatter3(instances(1:n, 1), instances(1:n, 2), instances(1:n, 3), 'r');
+hold on;
+scatter3(instances(n + 1:end, 1), instances(n + 1:end, 2), instances(n + 1:end, 3), 'b');
+xlabel('X Axis'); ylabel('Y Axis'); zlabel('Z Axis'); grid on;
+hold off;
