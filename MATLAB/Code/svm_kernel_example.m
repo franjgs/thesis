be792a9f6a46025f90cd69fcfc8ab86aef994378
Siddhant Goel <siddhantgoel@gmail.com>
2013-03-04 20:05:33 +0100

    Fix the code display in classification_methods

--- MATLAB/Code/svm_kernel_example.m
+++ MATLAB/Code/svm_kernel_example.m
@@ -3,14 +3,12 @@ n = 200; radius = 10; theta = 2 * pi * rand(n, 1);
 
 % points inside the circle
 r_in = rand(n, 1) * radius;
-x_in = r_in .* cos(theta);
-y_in = r_in .* sin(theta);
+x_in = r_in .* cos(theta); y_in = r_in .* sin(theta);
 label_in = ones(n, 1);
 
 % points outside the circle
-r_out = radius + 5 + rand(n, 1) * radius;
-x_out = r_out .* cos(theta);
-y_out = r_out .* sin(theta);
+r_out = radius + rand(n, 1) * radius;
+x_out = r_out .* cos(theta); y_out = r_out .* sin(theta);
 label_out = -1 * ones(n, 1);
 
 % final points
@@ -18,12 +16,14 @@ instances = [x_in y_in; x_out y_out;];
 labels = [label_in; label_out;];
 
 % plot the points in 2D
-% scatter(instances(1:200, 1), instances(1:200, 2), 'r');
-% hold on;
-% scatter(instances(201:400, 1), instances(201:400, 2), 'b');
-% hold off;
+figure(1);
+scatter(instances(1:200, 1), instances(1:200, 2), 'r');
+hold on;
+scatter(instances(201:400, 1), instances(201:400, 2), 'b');
+hold off;
 
 % transform the input to 3D and plot again
+figure(2);
 instances = [instances sqrt(power(instances(:, 1), 2) + power(instances(:, 2), 2))];
 scatter3(instances(1:n, 1), instances(1:n, 2), instances(1:n, 3), 'r');
 hold on;
