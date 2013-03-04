f9d42bf5cc60f3ef101cb0d1d0a26e5cabe59432
Siddhant Goel <siddhantgoel@gmail.com>
2013-03-04 18:27:49 +0100

    More information about kernel functions in the report

--- MATLAB/Code/svm_kernel_example.m
+++ MATLAB/Code/svm_kernel_example.m
@@ -16,5 +16,20 @@ x_out = r_out .* cos(theta);
 y_out = r_out .* sin(theta);
 label_out = -1 * ones(n, 1);
 
+% final points
 instances = [x_in y_in; x_out y_out;];
 labels = [label_in; label_out;];
+
+% plot the points in 2D
+scatter(instances(1:200, 1), instances(1:200, 2), 'r');
+hold on;
+scatter(instances(201:400, 1), instances(201:400, 2), 'b');
+hold off;
+
+% transform the input to 3D and plot again
+% instances = [instances ones(400, 1) .* (1:1:400)'];
+% scatter3(instances(1:200, 1), instances(1:200, 2), instances(1:200, 3), 'r');
+% hold on;
+% scatter3(instances(201:400, 1), instances(201:400, 2), instances(201:400, 3), 'b');
+% xlabel('X Axis'); ylabel('Y Axis'); zlabel('Z Axis'); grid on;
+% hold off;
