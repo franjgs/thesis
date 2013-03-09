6ed42ac35cf3c59115d867ed56c76fe7ec206b6f
Siddhant Goel <siddhantgoel@gmail.com>
2013-03-09 21:46:54 +0100

    Remove variable not being used in task fetch_from_reddit

--- Python/Code/web/ratings/tasks.py
+++ Python/Code/web/ratings/tasks.py
@@ -5,18 +5,11 @@ from web import settings
 
 @task
 def fetch_from_reddit():
-    count = 0
     for name in settings.SUBREDDITS:
         reddit = praw.Reddit(user_agent = name + ' user agent')
         subreddit = reddit.get_subreddit(name)
         submissions = subreddit.get_hot(limit = settings.MAX_STORIES)
         for x in submissions:
             if Story.objects.filter(id36 = x.id).count() == 0:
-                story = Story(
-                    id36 = x.id,
-                    content = x.title,
-                    label = 0,
-                    subreddit = name
-                )
+                story = Story(id36 = x.id, content = x.title, label = 0, subreddit = name)
                 story.save()
-                count = count + 1
